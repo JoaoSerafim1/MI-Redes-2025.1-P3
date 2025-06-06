03da819a318e6c5b45a5374ee0dddff657ca6ec3
JoaoSerafim1 <joaolucasacademico@gmail.com>
2025-06-05 21:39:30 -0300

    build: Updated property files

--- src/app_python/01_server/application/properties.py
+++ src/app_python/01_server/application/properties.py
@@ -7,14 +7,18 @@ maxClientThreads = 8
 #Maximo de threads simultaneos para requisicoes de outros servidores
 maxServerThreads = 8
 
-#Porta do broker MQTT e porta para requisicoes HTTP
+#Porta do broker MQTT, porta para requisicoes HTTP e porta para interacao com blockchain
 mqttPort = 1883
 httpPort = 8025
+blockChainNodePort = 7545
 
 #IP do broker MQTT de teste
 testBroker = 'broker.emqx.io'
 
 #Tempo em segundos antes e depois do horario exato marcado durante o qual um posto de recarga sera considerado como "ocupado"
 timeWindow = 7200
+
+#Tempo em segundos entre cada acao de sincronizacao
+syncWindow = 600
 
 ####################################################################################
